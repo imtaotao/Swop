d549dbd9feb77e7f371125b8fe427397a290de11
imtaotao <dwchentao@163.com>
2018-05-15 23:18:43 +0800

    feat: add ava test lib

--- build/index.js
+++ build/index.js
@@ -1,4 +1,8 @@
-export * from './swop';
-export * from './queue';
-export * from './tool';
-export * from './static_store';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./swop"), exports);
+tslib_1.__exportStar(require("./queue"), exports);
+tslib_1.__exportStar(require("./tool"), exports);
+tslib_1.__exportStar(require("./static_store"), exports);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9saWIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsaURBQXVCO0FBQ3ZCLGtEQUF3QjtBQUN4QixpREFBdUI7QUFDdkIseURBQStCIn0=
---
+++ test/customize_test.ts
@@ -0,0 +1,68 @@
+// 需要一个测试库
+import { Swop, CreateSwop } from '../build';
+import { InterfaceNames as I, DataContainerNames as D } from './interface_list'
+
+interface _Swop extends Swop<I, D>, D {}
+// const C = new Swop<I, D>();
+const C = CreateSwop<_Swop>({
+  json_parse: false,
+});
+(<any>window).c = C;
+
+C.send = () => {};
+
+C.create('dataOne', 'tt', true)
+ .create('dataTwo')
+ .create('dataThree');
+
+for (let i = 0; i < 5; i++) {
+  C.call('interfaceOne').then(([value, args]) => {
+    console.log(value)
+    setTimeout(_ => args.next(value, 'abc'), 1000)
+  })
+}
+
+const ids = C.get_funs('interfaceOne').map(val => val.id)
+
+C.use('*', (val) => {
+  if (typeof val.value === 'string') {
+    val.value = JSON.parse(val.value);
+  }
+})
+
+function polling (name:string) {
+  const div = document.createElement('div');
+  div.style.marginTop = '20px'; 
+  document.body.appendChild(div);
+
+  const remove = C[name].subscribe((new_v, old_v) => {
+    div.innerHTML = new_v.name;
+  });
+  
+  const p = C[name].polling();
+  
+  let i = 0;
+  setInterval(() => {
+    const _ids = C.get_funs(<any>name).map(val => val.id)
+    _ids.forEach((id:string, index:number) => {
+      C.response(<any>JSON.stringify({
+        origin_data: {
+          name: i + 's',
+        },
+        id,
+      }))
+    })
+    i++;
+  }, 1000)
+
+  i++
+  return {
+    remove,
+    p,
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  (<any>window).t = polling('dataTwo');
+  (<any>window).t = polling('dataThree');
+})
